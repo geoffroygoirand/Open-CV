81d238f47ab80f7a86bf139d420212a196827de6
geoffroygoirand <37084297+geoffroygoirand@users.noreply.github.com>
2022-03-24 09:45:29 +0100

    Create Firts-programme.py

---
+++ Firts-programme.py
@@ -0,0 +1,8 @@
+import cv2 as cv
+import sys
+
+# import des bibliothèques nécessaires à l'exécustion du code OpenCV
+
+img = cv.imread(cv.samples.findFile("First_night.jpg"))
+
+# Lecture de l'image par OpenCV
