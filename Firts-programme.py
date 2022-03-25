db65f2a225535679f4dfb4a116e07408eed871f0
geoffroygoirand <37084297+geoffroygoirand@users.noreply.github.com>
2022-03-25 09:13:45 +0100

    Update Firts-programme.py

--- Firts-programme.py
+++ Firts-programme.py
@@ -3,6 +3,15 @@ import sys
 
 # import des bibliothèques nécessaires à l'exécustion du code OpenCV
 
-img = cv.imread(cv.samples.findFile("First_night.jpg"))
+img = cv.imread("First_night.jpg")
 
 # Lecture de l'image par OpenCV
+
+
+# Affichage de l'image 
+
+from  matplotlib import pyplot as plt 
+
+plt.figure(figsize=(5,5))
+plt.imshow(img)
+plt.show()
